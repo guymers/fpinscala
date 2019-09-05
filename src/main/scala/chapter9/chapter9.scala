e2cd70f7289cf89ec4d7e577eb39da10e946c11d
Sam Guymer <sam@guymer.me>
2019-09-05 23:25:44 +1000

    chapter 9 redux

--- src/main/scala/chapter9/chapter9.scala
+++ src/main/scala/chapter9/chapter9.scala
@@ -2,71 +2,26 @@ package chapter9
 
 import chapter8.{Gen, Prop}
 
-import scala.annotation.tailrec
 import scala.util.matching.Regex
 
 trait Parsers[ParseError, Parser[+_]] { self =>
 
-  val digits: Parser[Int] = map(regex("[0-9]+".r))(_.toInt)
+  val digits: Parser[Int] = regex("[0-9]+".r).map(_.toInt)
 
-  def char(c: Char): Parser[Char] =
-    map(string(c.toString))(_.charAt(0))
+  def char(c: Char): Parser[Char] = string(c.toString).map(_.charAt(0))
+
+  def run[A](p: Parser[A])(input: String): Either[ParseError, A]
+
+  def succeed[A](a: A): Parser[A] = string("").map(_ => a)
+
   implicit def string(s: String): Parser[String]
   implicit def regex(r: Regex): Parser[String]
 
-  def succeed[A](a: A): Parser[A] =
-    map(string(""))(_ => a) //string("") map (_ => a)
-
   def slice[A](p: Parser[A]): Parser[String]
 
-  def or[A](s1: Parser[A], s2: => Parser[A]): Parser[A]
-
-  // 9.1
-  def map2_1[A, B, C](p: Parser[A], p2: => Parser[B])(f: (A, B) => C): Parser[C] =
-    map(product(p, p2)) { case (a, b) => f(a, b) }
-
-  // 9.3
-  private def prepend[A](p1: Parser[A], p2: => Parser[List[A]]): Parser[List[A]] = {
-    map2(p1, p2) { case (a, b) => a :: b }
-  }
-
-  def many[A](p: Parser[A]): Parser[List[A]] =
-    or(prepend(p, many(p)), succeed(Nil))
-
-  def many1[A](p: Parser[A]): Parser[List[A]] =
-    prepend(p, many(p))
-
-  // 9.4
-  def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]] = {
-    @tailrec
-    def go(l: Parser[List[A]], count: Int): Parser[List[A]] =
-      if (count <= 0) l
-      else go(prepend(p, l), count - 1)
-    go(succeed(Nil), n).map(_.reverse)
-  }
+  def or[A](p1: Parser[A], p2: => Parser[A]): Parser[A]
 
   def flatMap[A, B](p: Parser[A])(f: A => Parser[B]): Parser[B]
-
-  // 9.6
-  def numChars[A](p: Parser[A]): Parser[List[A]] =
-    flatMap(digits)(n => listOfN(n, p))
-
-  // 9.7
-  def product[A, B](p: Parser[A], p2: => Parser[B]): Parser[(A, B)] =
-    flatMap(p) { pA =>
-      flatMap(p2)(pB => succeed((pA, pB)))
-    }
-
-  def map2[A, B, C](p: Parser[A], p2: => Parser[B])(f: (A, B) => C): Parser[C] =
-    flatMap(p) { pA =>
-      flatMap(p2)(pB => succeed(f(pA, pB)))
-    }
-
-  // 9.8
-  def map[A, B](p: Parser[A])(f: A => B): Parser[B] =
-    flatMap(p)(a => succeed(f(a)))
-
-  def run[A](p: Parser[A])(input: String): Either[ParseError, A]
 
   implicit def operators[A](p: Parser[A]): ParserOps[A] = ParserOps[A](p)
   implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] = ParserOps(f(a))
@@ -76,11 +31,11 @@ trait Parsers[ParseError, Parser[+_]] { self =>
     def or[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
 
     def many: Parser[List[A]] = self.many(p)
-    def map[B >: A](f: A => B): Parser[B] = self.map(p)(f)
-    def flatMap[B >: A](f: A => Parser[B]): Parser[B] = self.flatMap(p)(f)
+    def map[B](f: A => B): Parser[B] = self.map(p)(f)
+    def flatMap[B](f: A => Parser[B]): Parser[B] = self.flatMap(p)(f)
 
-    def **[B >: A](p2: Parser[B]): Parser[(A, B)] = self.product(p, p2)
-    def product[B >: A](p2: Parser[B]): Parser[(A, B)] = self.product(p, p2)
+    def **[B](p2: Parser[B]): Parser[(A, B)] = self.product(p, p2)
+    def product[B](p2: Parser[B]): Parser[(A, B)] = self.product(p, p2)
   }
 
   object Laws {
@@ -88,6 +43,45 @@ trait Parsers[ParseError, Parser[+_]] { self =>
       Prop.forAll(in)(s => run(p1)(s) == run(p2)(s))
     def mapLaw[A](p: Parser[A])(in: Gen[String]): Prop =
       equal(p, p.map(a => a))(in)
+  }
+
+  // 9.1
+  def map2[A, B, C](p1: Parser[A], p2: => Parser[B])(f: (A, B) => C): Parser[C] = {
+    product(p1, p2).map { case (a, b) => f(a, b) }
+  }
+
+  def many1[A](p: Parser[A]): Parser[::[A]] = map2(p, many(p))(::(_, _))
+
+  // 9.2 laws
+
+  // 9.3
+  def many[A](p: Parser[A]): Parser[List[A]] = {
+    map2(p, many(p))(_ :: _) or succeed(Nil)
+  }
+
+  // 9.4
+  def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]] = {
+    if (n <= 0) succeed(Nil)
+    else map2(p, listOfN(n - 1, p))(_ :: _)
+  }
+
+  // 9.6
+  def numChars[A](p: Parser[A]): Parser[List[A]] = {
+    digits.flatMap(n => listOfN(n, p))
+  }
+
+  // 9.7
+  def product[A, B](p1: Parser[A], p2: => Parser[B]): Parser[(A, B)] = {
+    p1.flatMap(a => p2.map(b => (a, b)))
+  }
+
+  def map2FlatMap[A, B, C](p1: Parser[A], p2: => Parser[B])(f: (A, B) => C): Parser[C] = {
+    p1.flatMap(a => p2.map(b => f(a, b)))
+  }
+
+  // 9.8
+  def map[A, B](p: Parser[A])(f: A => B): Parser[B] = {
+    p.flatMap(a => succeed(f(a)))
   }
 }
 
@@ -103,6 +97,12 @@ object JsonParser {
   }
 
   val parsers = new Parsers[ParseError, JsonParser] {
+    override def succeed[A](a: A): JsonParser[A] = new JsonParser[A] {
+      override def parse(input: String): ParseResult[A] = {
+        Right(ParseSuccess(a, "", input))
+      }
+      override def toString: String = s"succeed($a)"
+    }
     override implicit def string(s: String): JsonParser[String] = new JsonParser[String] {
       override def parse(input: String): ParseResult[String] = {
         if (input.startsWith(s)) {
@@ -114,53 +114,36 @@ object JsonParser {
     }
     override implicit def regex(r: Regex): JsonParser[String] = new JsonParser[String] {
       override def parse(input: String): ParseResult[String] = {
-        val m = r.findPrefixOf(input)
-        m.fold[ParseResult[String]] {
-          Left(s"Regex '$r' does not match")
-        } { v =>
-          Right(ParseSuccess(v, v, input.drop(v.length)))
+        r.findPrefixOf(input).toRight(s"Regex '$r' does not match").map { v =>
+          ParseSuccess(v, v, input.drop(v.length))
         }
       }
       override def toString: String = s"regex($r)"
     }
     override def slice[A](p: JsonParser[A]): JsonParser[String] = new JsonParser[String] {
       override def parse(input: String): ParseResult[String] = {
-        p.parse(input) match {
-          case Left(v) => Left(v)
-          case Right(ParseSuccess(_, s, remainingInput)) => Right(ParseSuccess(s, s, remainingInput))
+        p.parse(input).map { case ParseSuccess(_, s, remainingInput) =>
+          ParseSuccess(s, s, remainingInput)
         }
       }
       override def toString: String = s"slice($p)"
     }
     override def or[A](p1: JsonParser[A], p2: => JsonParser[A]): JsonParser[A] = new JsonParser[A] {
       override def parse(input: String): ParseResult[A] = {
-        p1.parse(input) match {
-          case Left(_) => p2.parse(input)
-          case r => r
-        }
+        p1.parse(input).left.flatMap(_ => p2.parse(input))
       }
       override def toString: String = s"or($p1,$p2)"
     }
-    override def succeed[A](a: A): JsonParser[A] = new JsonParser[A] {
-      override def parse(input: String): ParseResult[A] = {
-        Right(ParseSuccess(a, "", input))
-      }
-      override def toString: String = s"succeed $a"
-    }
     override def flatMap[A, B](p: JsonParser[A])(f: A => JsonParser[B]): JsonParser[B] = new JsonParser[B] {
       override def parse(input: String): ParseResult[B] = {
-        p.parse(input) match {
-          case Left(v) => Left(v)
-          case Right(ParseSuccess(v, _, remainingInput)) => f(v).parse(remainingInput)
+        p.parse(input).flatMap { case ParseSuccess(v, _, remainingInput) =>
+          f(v).parse(remainingInput)
         }
       }
       override def toString: String = s"flatMap($p)"
     }
     override def run[A](p: JsonParser[A])(input: String): Either[ParseError, A] =
-      p.parse(input) match {
-        case Left(v) => Left(v)
-        case Right(ParseSuccess(v, _, _)) => Right(v)
-      }
+      p.parse(input).map(_.value)
   }
 
   val json: JsonParser[JSON] = {
@@ -173,29 +156,34 @@ object JsonParser {
     val comma = char(',')
     val colon = char(':')
 
-    val jsonNull = map(string("null"))(_ => JNull)
-    val jsonNumber = map(regex("""[0-9.]+""".r))(v => JNumber(v.toDouble))
-    val jsonString = map(quotedString)(JString)
-    val jsonBool = map(or(string("true"), string("false")))(v => JBool(v == "true"))
-    val primitive = or(jsonNull, or(jsonNumber, or(jsonString, jsonBool)))
+    val jsonNull = string("null").map(_ => JNull)
+    val jsonNumber = regex("""[0-9.]+""".r).flatMap { s =>
+      val d = s.toDouble
+      regex("""e[0-9]+""".r).map(s => s.drop(1).toInt).or(succeed(0)).map { e =>
+        d * Math.pow(10, e.toDouble)
+      }
+    }.map(JNumber)
+    val jsonString = quotedString.map(JString)
+    val jsonBool = (string("true") or string("false")).map(v => JBool(v == "true"))
+    val primitive = jsonNull or jsonNumber or jsonString or jsonBool
 
     lazy val jsonArray = flatMap(char('[')) { _ =>
       lazy val _jsonValue = jsonValue
-      val arrayItemComma = map(product(_jsonValue, comma))(_._1)
-      val finalArrayItem = map(product(_jsonValue, char(']')))(_._1)
-      val emptyArray = map(char(']'))(_ => List.empty[JSON])
-      val jsonList = map(product(many(arrayItemComma), finalArrayItem))(v => v._1 :+ v._2)
-      map(or(emptyArray, jsonList))(v => JArray(v.toIndexedSeq))
+      val arrayItemComma = product(_jsonValue, comma).map(_._1)
+      val finalArrayItem = product(_jsonValue, char(']')).map(_._1)
+      val emptyArray = char(']').map(_ => List.empty[JSON])
+      val jsonList = product(many(arrayItemComma), finalArrayItem).map(v => v._1 :+ v._2)
+      (emptyArray or jsonList).map(v => JArray(v.toIndexedSeq))
     }
 
     lazy val jsonObject: JsonParser[JObject] = flatMap(char('{')) { _ =>
       lazy val _jsonValue = jsonValue
       val objectItem = trimWhitespace(product(map(product(quotedString, trimWhitespace(colon)))(_._1), _jsonValue))
-      val objectItemComma = map(product(objectItem, comma))(_._1)
-      val finalObjectItem = map(product(objectItem, char('}')))(_._1)
-      val emptyObject = map(char('}'))(_ => Map.empty[String, JSON])
-      val obj = map(product(many(objectItemComma), finalObjectItem))(v => v._1 :+ v._2)
-      map(or(emptyObject, obj))(v => JObject(v.toMap))
+      val objectItemComma = product(objectItem, comma).map(_._1)
+      val finalObjectItem = product(objectItem, char('}')).map(_._1)
+      val emptyObject = char('}').map(_ => Map.empty[String, JSON])
+      val obj = product(many(objectItemComma), finalObjectItem).map(v => v._1 :+ v._2)
+      (emptyObject or obj).map(v => JObject(v.toMap))
     }
 
     def jsonValue: JsonParser[JSON] = trimWhitespace(or(primitive, or(jsonArray, jsonObject)))
--- src/test/scala/chapter9/Chapter9Test.scala
+++ src/test/scala/chapter9/Chapter9Test.scala
@@ -1,51 +1,74 @@
 package chapter9
 
-import org.scalatest.FlatSpec
+import org.scalatest.FreeSpec
 
-class Chapter9Test extends FlatSpec {
+class Chapter9Test extends FreeSpec {
   import chapter9.JSON._
 
-  "json parser" should "parse primitives" in {
-    assert(JsonParser.run("null") == Right(JNull))
-    assert(JsonParser.run("true") == Right(JBool(true)))
-    assert(JsonParser.run("false") == Right(JBool(false)))
-    assert(JsonParser.run("123") == Right(JNumber(123D)))
-    assert(JsonParser.run("123.45") == Right(JNumber(123.45D)))
-    assert(JsonParser.run(""""string"""") == Right(JString("string")))
-  }
+  "json parser" - {
+    "parse primitives" in {
+      assert(JsonParser.run("null") == Right(JNull))
+      assert(JsonParser.run("true") == Right(JBool(true)))
+      assert(JsonParser.run("false") == Right(JBool(false)))
+      assert(JsonParser.run("123") == Right(JNumber(123D)))
+      assert(JsonParser.run("123.45") == Right(JNumber(123.45D)))
+      assert(JsonParser.run(""""string"""") == Right(JString("string")))
+    }
 
-  "json parser" should "parse arrays" in {
-    assert(JsonParser.run("[]") == Right(JArray(Vector.empty)))
-    assert(JsonParser.run("[123]") == Right(JArray(Vector(JNumber(123D)))))
-    assert(JsonParser.run("""[123, "string"]""") == Right(JArray(Vector(JNumber(123D), JString("string")))))
-    assert(JsonParser.run("""[123"string"]""").isLeft)
-    assert(JsonParser.run("""[123 "string"]""").isLeft)
+    "parse arrays" in {
+      assert(JsonParser.run("[]") == Right(JArray(Vector.empty)))
+      assert(JsonParser.run("[123]") == Right(JArray(Vector(JNumber(123D)))))
+      assert(JsonParser.run("""[123, "string"]""") == Right(JArray(Vector(JNumber(123D), JString("string")))))
+      assert(JsonParser.run("""[123"string"]""").isLeft)
+      assert(JsonParser.run("""[123 "string"]""").isLeft)
 
-    assert(
-      JsonParser.run("""[[123, 456], ["a", "b"]]""") == Right(
-        JArray(Vector(
-          JArray(Vector(JNumber(123D), JNumber(456D))),
-          JArray(Vector(JString("a"), JString("b")))
-        ))
+      assert(
+        JsonParser.run("""[[123, 456], ["a", "b"]]""") == Right(
+          JArray(Vector(
+            JArray(Vector(JNumber(123D), JNumber(456D))),
+            JArray(Vector(JString("a"), JString("b")))
+          ))
+        )
       )
-    )
-  }
+    }
 
-  "json parser" should "parse objects" in {
-    assert(JsonParser.run("{}") == Right(JObject(Map.empty)))
-    assert(JsonParser.run("""{"key" : "value"}""") == Right(JObject(Map("key" -> JString("value")))))
-    assert(
-      JsonParser.run("""{"key": "value", "num": 123}""") == Right(
-        JObject(Map("key" -> JString("value"), "num" -> JNumber(123D)))
+    "parse objects" in {
+      assert(JsonParser.run("{}") == Right(JObject(Map.empty)))
+      assert(JsonParser.run("""{"key" : "value"}""") == Right(JObject(Map("key" -> JString("value")))))
+      assert(
+        JsonParser.run("""{"key": "value", "num": 123}""") == Right(
+          JObject(Map("key" -> JString("value"), "num" -> JNumber(123D)))
+        )
       )
-    )
-    assert(JsonParser.run("""{"key":"value""num":123}""").isLeft)
-    assert(JsonParser.run("""{"key" : "value" "num" : 123}""").isLeft)
+      assert(JsonParser.run("""{"key":"value""num":123}""").isLeft)
+      assert(JsonParser.run("""{"key" : "value" "num" : 123}""").isLeft)
 
-    assert(
-      JsonParser.run("""{"key":["a","b"]}""") == Right(
-        JObject(Map("key" -> JArray(Vector(JString("a"), JString("b")))))
+      assert(
+        JsonParser.run("""{"key":["a","b"]}""") == Right(
+          JObject(Map("key" -> JArray(Vector(JString("a"), JString("b")))))
+        )
       )
-    )
+    }
+
+    "parse" in {
+      val json = """
+        {
+          "Company name": "Microsoft Corporation",
+          "Ticker": "MSFT",
+          "Active": true,
+          "Price": 30.66,
+          "Shares outstanding": 8.38e9,
+          "Related companies": [ "HPQ", "IBM", "YHOO", "DELL", "GOOG" ]
+        }
+      """
+      assert(JsonParser.run(json) == Right(JObject(Map(
+        "Company name" -> JString("Microsoft Corporation"),
+        "Ticker" -> JString("MSFT"),
+        "Active" -> JBool(true),
+        "Price" -> JNumber(30.66),
+        "Shares outstanding" -> JNumber(8.380000000000001E9),
+        "Related companies" -> JArray(Vector(JString("HPQ"), JString("IBM"), JString("YHOO"), JString("DELL"), JString("GOOG"))),
+      ))))
+    }
   }
 }
