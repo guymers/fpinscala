af972b241d91e5b462a698cb97573f59323e1327
Sam Guymer <sam@guymer.me>
2019-09-12 23:18:07 +1000

    chapter 10 redux

--- src/main/scala/chapter10/chapter10.scala
+++ src/main/scala/chapter10/chapter10.scala
@@ -3,93 +3,113 @@ package chapter10
 import chapter8.{Gen, Prop}
 
 trait Monoid[A] {
-  def op(a1: A, a2: A): A
   def zero: A
+  def op(a1: A, a2: A): A
 }
 
 object Monoid {
 
   val stringConcatenation: Monoid[String] = new Monoid[String] {
-    override def op(a1: String, a2: String): String = a1 + a2
-    override def zero: String = ""
+    override val zero: String = ""
+    override def op(a1: String, a2: String): String = a1 concat a2
+  }
+
+  def listConcatenation[A]: Monoid[List[A]] = new Monoid[List[A]] {
+    override val zero: List[A] = Nil
+    override def op(a1: List[A], a2: List[A]): List[A] = a1 ::: a2
   }
 
   // 10.1
   val intAddition: Monoid[Int] = new Monoid[Int] {
+    override val zero: Int = 0
     override def op(a1: Int, a2: Int): Int = a1 + a2
-    override def zero: Int = 0
   }
   val intMultiplication: Monoid[Int] = new Monoid[Int] {
+    override val zero: Int = 1
     override def op(a1: Int, a2: Int): Int = a1 * a2
-    override def zero: Int = 1
   }
   val booleanOr: Monoid[Boolean] = new Monoid[Boolean] {
+    override val zero: Boolean = false
     override def op(a1: Boolean, a2: Boolean): Boolean = a1 || a2
-    override def zero: Boolean = false
   }
   val booleanAnd: Monoid[Boolean] = new Monoid[Boolean] {
+    override val zero: Boolean = true
     override def op(a1: Boolean, a2: Boolean): Boolean = a1 && a2
-    override def zero: Boolean = true
   }
 
   // 10.2
   def optionMonoid[A]: Monoid[Option[A]] = new Monoid[Option[A]] {
+    override val zero: Option[A] = None
     override def op(a1: Option[A], a2: Option[A]): Option[A] = a1 orElse a2
-    override def zero: Option[A] = None
   }
 
   // 10.3
   def endoMonoid[A]: Monoid[A => A] = new Monoid[A => A] {
+    override val zero: A => A = identity
     override def op(a1: A => A, a2: A => A): A => A = a1 andThen a2
-    override def zero: A => A = identity
   }
 
   // 10.4
-  def monoidLaws[A](m: Monoid[A], gen: Gen[A]): Prop = {
-    val leftIdentity = Prop.forAll(gen) { x =>
-      m.op(x, m.zero) == x
+  def monoidLaws[A](m: Monoid[A], gen: Gen[A])(implicit eq: (A, A) => Boolean = (a1: A, a2: A) => a1 == a2): Prop = {
+    val leftIdentity = Prop.forAll(gen) { a =>
+      eq(m.op(m.zero, a), a)
     }
-    val rightIdentity = Prop.forAll(gen) { x =>
-      m.op(m.zero, x) == x
+
+    val rightIdentity = Prop.forAll(gen) { a =>
+      eq(m.op(a, m.zero), a)
     }
-    // ANS: flatmap instead of product
-    val tripleGen = (gen ** gen ** gen).map(v => (v._1._1, v._1._2, v._2))
-    val associative = Prop.forAll(tripleGen) { case (x, y, z) =>
-      m.op(m.op(x, y), z) == m.op(x, m.op(y, z))
+
+    val associativity = Prop.forAll(gen ** gen ** gen) { case ((a, b), c) =>
+      eq(m.op(m.op(a, b), c), m.op(a, m.op(b, c)))
     }
-    leftIdentity && rightIdentity && associative
+
+    leftIdentity && rightIdentity && associativity
   }
 
   def concatenate[A](as: List[A], m: Monoid[A]): A =
     as.foldLeft(m.zero)(m.op)
 
   // 10.5
-  def foldMap[A, B](as: List[A], m: Monoid[B])(f: A => B): B =
-    as.foldLeft(m.zero) { case (acc, v) => m.op(acc, f(v)) }
+  def foldMap[A, B](as: Seq[A], m: Monoid[B])(f: A => B): B = {
+    // concatenate(as.map(f), m)
+    as.foldLeft(m.zero) { case (b, a) => m.op(b, f(a)) }
+  }
 
   // 10.6
-  // ANS: currying B => A => B
-  def foldLeft[A, B](as: List[A], m: Monoid[B])(z: B)(op: (B, A) => B): B =
-    foldMap(as, endoMonoid[B])(a => b => op(b, a))(z)
+  def foldRight[A, B](as: List[A])(z: B)(f: (A, B) => B): B = {
+    foldMap(as, endoMonoid[B])(f.curried)(z)
+  }
 
   // 10.7
   def foldMapV[A, B](v: IndexedSeq[A], m: Monoid[B])(f: A => B): B = {
-    val len = v.length
-    len match {
-      case 0 => m.zero
-      case 1 => f(v(0))
-      case _ =>
-        val (a, b) = v.splitAt(len / 2)
-        m.op(foldMapV(a, m)(f), foldMapV(b, m)(f))
+    val length = v.length
+    if (length <= 0) {
+      m.zero
+    } else if (length == 1) {
+      f(v.head) // identity law
+    } else {
+      val (l, r) = v.splitAt(length / 2)
+      m.op(foldMapV(l, m)(f), foldMapV(r, m)(f))
     }
   }
 
   // 10.8
+  import chapter7.nonblocking._
+  def par[A](m: Monoid[A]): Monoid[Par[A]] = new Monoid[Par[A]] {
+    override val zero: Par[A] = Par.unit(m.zero)
+    override def op(a1: Par[A], a2: Par[A]): Par[A] = Par.map2(a1, a2)(m.op)
+  }
+  def parFoldMap[A, B](v: IndexedSeq[A], m: Monoid[B])(f: A => B): Par[B] = {
+    //Par.parMap(v)(f).flatMap { bs =>
+    //  foldMapV(bs, par(m))(b => Par.lazyUnit(b))
+    //}
+    ???
+  }
 
   // 10.9
-  // ANS: tracks max and min
   type PreviousValue[A] = (Boolean, Option[A])
   val sortedIntMonoid: Monoid[PreviousValue[Int]] = new Monoid[PreviousValue[Int]] {
+    override val zero: PreviousValue[Int] = (true, None)
     override def op(a1: PreviousValue[Int], a2: PreviousValue[Int]): PreviousValue[Int] = {
       val sorted = for {
         v1 <- a1._2
@@ -99,97 +119,70 @@ object Monoid {
       }
       (sorted.getOrElse(true), a2._2)
     }
-    override def zero: PreviousValue[Int] = (true, None)
   }
-  def isSorted(v: IndexedSeq[Int]): Boolean =
+  def isSorted(v: IndexedSeq[Int]): Boolean = {
     foldMap(v.toList, sortedIntMonoid)(v => (false, Some(v)))._1
+  }
 
   // 10.10
-  // ANS: assumes individual characters which simplifies
   val wcMonoid: Monoid[WC] = new Monoid[WC] {
-    override def op(a1: WC, a2: WC): WC = (a1, a2) match {
-      case (Stub(c1), Stub(c2)) =>
-        toWC(c1 + c2)
-      case (Stub(c1), Part(lStub, words, rStub)) =>
-        if (c1.isEmpty || lStub.isEmpty) {
-          Part(c1 + lStub, words, rStub)
-        } else {
-          op(toWC(c1 + lStub), Part("", words, rStub))
-        }
-      case (Part(lStub, words, rStub), Stub(c2)) =>
-        if (c2.isEmpty || rStub.isEmpty) {
-          Part(lStub, words, rStub + c2)
-        } else {
-          op(Part(lStub, words, ""), toWC(rStub + c2))
-        }
-      case (Part(lStub1, words1, rStub1), Part(lStub2, words2, rStub2)) =>
-        val middleWord = if (!rStub1.isEmpty || !lStub2.isEmpty) 1 else 0
-        Part(lStub1, words1 + words2 + middleWord, rStub2)
-    }
-    override def zero: WC = Stub("")
-  }
-
-  def toWC(str: String): WC = {
-    if (str.isEmpty) {
-      Stub(str)
-    } else {
-      val whitespace = """\s"""
-      val leadingWhitespace = str(0).toString.matches(whitespace)
-      val trailingWhitespace = str(str.length - 1).toString.matches(whitespace)
-
-      val parts = str.split("""\s+""").filter(!_.isEmpty)
-      val words = parts.length
-      if (words > 1 || (words == 1 && (leadingWhitespace || trailingWhitespace))) {
-        val firstPart = parts(0)
-        val (lStub, lWord) = if (leadingWhitespace) ("", 0) else (firstPart, 1)
-        val lastPart = parts(words - 1)
-        val (rStub, rWord) = if (trailingWhitespace) ("", 0) else (lastPart, 1)
-        Part(lStub, words - lWord - rWord, rStub)
-      } else {
-        Stub(str)
+    override val zero: WC = Stub("")
+    override def op(a1: WC, a2: WC): WC = {
+      (a1, a2) match {
+        case (Stub(c1), Stub(c2)) => Stub(c1 concat c2)
+        case (Stub(c), Part(l, count, r)) => Part(c concat l, count, r)
+        case (Part(l, count, r), Stub(c)) => Part(l, count, r concat c)
+        case (Part(l, count1, m1), Part(m2, count2, r)) =>
+          val count = count1 + count2 + (if (m1.nonEmpty || m2.nonEmpty) 1 else 0)
+          Part(l, count, r)
       }
     }
   }
 
   // 10.11
-  def countWords(s: String): Int =
-    foldMapV(s, wcMonoid)(c => toWC(c.toString)) match {
-      case Stub(_) => 0
-      case Part(_, words, _) => words
+  def countWords(s: String): Int = {
+    def wc(c: Char): WC = {
+      if (c.isWhitespace) Part("", 0, "")
+      else Stub(c.toString)
     }
+
+    def count(s: String): Int = if (s.nonEmpty) 1 else 0
+    foldMapV(s, wcMonoid)(wc) match {
+      case Stub(s) => count(s)
+      case Part(l, words, r) => count(l) + words + count(r)
+    }
+  }
 
   // 10.16
   def productMonoid[A, B](A: Monoid[A], B: Monoid[B]): Monoid[(A, B)] = new Monoid[(A, B)] {
-    override def op(a1: (A, B), a2: (A, B)): (A, B) =
+    override val zero: (A, B) = (A.zero, B.zero)
+    override def op(a1: (A, B), a2: (A, B)): (A, B) = {
       (A.op(a1._1, a2._1), B.op(a1._2, a2._2))
-    override def zero: (A, B) =
-      (A.zero, B.zero)
+    }
   }
 
   def mapMergeMonoid[K, V](V: Monoid[V]): Monoid[Map[K, V]] = new Monoid[Map[K, V]] {
-    def zero = Map[K, V]()
-    def op(a: Map[K, V], b: Map[K, V]) =
+    override val zero: Map[K, V] = Map.empty
+    override def op(a: Map[K, V], b: Map[K, V]): Map[K, V] = {
       (a.keySet ++ b.keySet).foldLeft(zero) { (acc, k) =>
         acc.updated(
           k,
           V.op(a.getOrElse(k, V.zero), b.getOrElse(k, V.zero))
         )
       }
+    }
   }
 
   // 10.17
   def functionMonoid[A, B](B: Monoid[B]): Monoid[A => B] = new Monoid[A => B] {
-    override def op(a1: A => B, a2: A => B): A => B =
-      a => B.op(a1(a), a2(a))
-    override def zero: A => B = a => B.zero
+    override val zero: A => B = _ => B.zero
+    override def op(a1: A => B, a2: A => B): A => B = a => B.op(a1(a), a2(a))
   }
 
   // 10.18
   def bag[A](as: IndexedSeq[A]): Map[A, Int] = {
-    val m = mapMergeMonoid[A, Int](intAddition)
-    foldMap(as.toList, m)(a => Map(a -> 1))
+    foldMap(as, mapMergeMonoid[A, Int](intAddition))(a => Map(a -> 1))
   }
-
 }
 
 sealed trait WC extends Product with Serializable
@@ -200,14 +193,17 @@ case class Part(lStub: String, words: Int, rStub: String) extends WC
 trait Foldable[F[_]] {
   def foldRight[A, B](as: F[A])(z: B)(f: (A, B) => B): B
   def foldLeft[A, B](as: F[A])(z: B)(f: (B, A) => B): B
-  def foldMap[A, B](as: F[A])(f: A => B)(mb: Monoid[B]): B =
+  def foldMap[A, B](as: F[A])(f: A => B)(mb: Monoid[B]): B = {
     foldLeft(as)(mb.zero) { case (acc, v) => mb.op(acc, f(v)) }
-  def concatenate[A](as: F[A])(m: Monoid[A]): A =
+  }
+  def concatenate[A](as: F[A])(m: Monoid[A]): A = {
     foldLeft(as)(m.zero)(m.op)
+  }
 
   // 10.15
-  def toList[A](fa: F[A]): List[A] =
-    foldRight(fa)(List.empty[A]) { case (a, b) => a :: b }
+  def toList[A](fa: F[A]): List[A] = {
+    foldRight(fa)(List.empty[A])(_ :: _)
+  }
 }
 
 object Foldable {
--- src/test/scala/chapter10/Chapter10Test.scala
+++ src/test/scala/chapter10/Chapter10Test.scala
@@ -1,77 +1,80 @@
 package chapter10
 
-import chapter8._
-import org.scalatest.{EitherValues, FlatSpec}
-
 import scala.util.Random
 
-class Chapter10Test extends FlatSpec with EitherValues {
+import chapter8._
+import org.scalatest.FreeSpec
+
+class Chapter10Test extends FreeSpec {
 
   private val intGen = Gen.choose(-2000000, 2000000)
   private val booleanGen = Gen.boolean
 
-  "monoid laws" should "hold for int addition" in {
-    val prop = Monoid.monoidLaws(Monoid.intAddition, intGen)
-    Prop.run(prop)
+  "monoid laws" - {
+    "hold for int addition" in {
+      val prop = Monoid.monoidLaws(Monoid.intAddition, intGen)
+      Prop.run(prop)
+    }
+
+    "hold for int multiplication" in {
+      val prop = Monoid.monoidLaws(Monoid.intMultiplication, intGen)
+      Prop.run(prop)
+    }
+
+    "hold for boolean or" in {
+      val prop = Monoid.monoidLaws(Monoid.booleanOr, booleanGen)
+      Prop.run(prop)
+    }
+
+    "hold for boolean and" in {
+      val prop = Monoid.monoidLaws(Monoid.booleanAnd, booleanGen)
+      Prop.run(prop)
+    }
+
+    "hold for options" in {
+      val prop = Monoid.monoidLaws(
+        Monoid.optionMonoid[Int],
+        (booleanGen ** booleanGen ** intGen).map { case ((b1, b2), i) => if (!b1 && !b2) None else Some(i) }
+      )
+      Prop.run(prop)
+    }
+
+    "hold for endo" in {
+      val prop = Monoid.monoidLaws(
+        Monoid.endoMonoid[Int],
+        intGen.map(v => (i: Int) => v + i)
+      )((a1, a2) => a1(1) == a2(1)) // bit shit
+      Prop.run(prop)
+    }
   }
 
-  "monoid laws" should "hold for int multiplication" in {
-    val prop = Monoid.monoidLaws(Monoid.intMultiplication, intGen)
-    Prop.run(prop)
+  "foldMapV" - {
+    "handle empty sequences" in {
+      val v = IndexedSeq.empty[String]
+      val ans = Monoid.foldMapV(v, Monoid.stringConcatenation)(identity)
+      assert(ans === "")
+    }
+
+    "handle a single item" in {
+      val v = IndexedSeq("lorem")
+      val ans = Monoid.foldMapV(v, Monoid.stringConcatenation)(identity)
+      assert(ans === "lorem")
+    }
+
+    "handle even sequences" in {
+      val v = IndexedSeq("lorem", "ipsum", "dolor", "sit")
+      val ans = Monoid.foldMapV(v, Monoid.stringConcatenation)(identity)
+      assert(ans === "loremipsumdolorsit")
+    }
+
+    "handle odd sequences" in {
+      val v = IndexedSeq("lorem", "ipsum", "dolor", "sit", "blah")
+      val ans = Monoid.foldMapV(v, Monoid.stringConcatenation)(identity)
+      assert(ans === "loremipsumdolorsitblah")
+    }
   }
 
-  "monoid laws" should "hold for boolean or" in {
-    val prop = Monoid.monoidLaws(Monoid.booleanOr, booleanGen)
-    Prop.run(prop)
-  }
-
-  "monoid laws" should "hold for boolean and" in {
-    val prop = Monoid.monoidLaws(Monoid.booleanAnd, booleanGen)
-    Prop.run(prop)
-  }
-
-  "monoid laws" should "hold for options" in {
-    val prop = Monoid.monoidLaws(
-      Monoid.optionMonoid[Int],
-      (booleanGen ** booleanGen ** intGen).map(v => if (!v._1._1 && !v._1._2) None else Some(v._2))
-    )
-    Prop.run(prop)
-  }
-
-//  "monoid laws" should "hold for endo" in {
-//    // TODO function equality?
-//    val prop = Monoid.monoidLaws(
-//      Monoid.endoMonoid[Int],
-//      intGen.map(v => (i: Int) => v + i)
-//    )
-//    run(prop)
-//  }
-
-  "foldMapV" should "handle empty sequences" in {
-    val v = IndexedSeq.empty[String]
-    val ans = Monoid.foldMapV(v, Monoid.stringConcatenation)(identity)
-    assert(ans === "")
-  }
-
-  "foldMapV" should "handle a single item" in {
-    val v = IndexedSeq("lorem")
-    val ans = Monoid.foldMapV(v, Monoid.stringConcatenation)(identity)
-    assert(ans === "lorem")
-  }
-
-  "foldMapV" should "handle even sequences" in {
-    val v = IndexedSeq("lorem", "ipsum", "dolor", "sit")
-    val ans = Monoid.foldMapV(v, Monoid.stringConcatenation)(identity)
-    assert(ans === "loremipsumdolorsit")
-  }
-
-  "foldMapV" should "handle odd sequences" in {
-    val v = IndexedSeq("lorem", "ipsum", "dolor", "sit", "blah")
-    val ans = Monoid.foldMapV(v, Monoid.stringConcatenation)(identity)
-    assert(ans === "loremipsumdolorsitblah")
-  }
-
-  "sorted int" should "" in {
+  "sorted int" in {
     assert(Monoid.isSorted(IndexedSeq()) === true)
     assert(Monoid.isSorted(IndexedSeq(1)) === true)
     assert(Monoid.isSorted(IndexedSeq(1, 2, 3, 4)) === true)
@@ -79,71 +82,72 @@ class Chapter10Test extends FlatSpec with EitherValues {
 //    assert(Monoid.isSorted(IndexedSeq(4, 3, 2, 1)) === true)
   }
 
-  "monoid laws" should "hold for WC" in {
-    val prop = Monoid.monoidLaws(
-      Monoid.wcMonoid,
-      for {
-        b1 <- booleanGen
-        b2 <- booleanGen
-        randomStrSeed <- Gen.choose(1, 200000)
-        strLength <- Gen.choose(0, 10)
-        spaceLocation <- Gen.choose(0, 10)
-        numWords <- Gen.choose(0, 4)
-      } yield {
-        val createStub = b1 && b2
-        val r = new Random(randomStrSeed)
-        val str = r.nextString(strLength).replaceAll("""\s""", "")
+  "monoid laws" - {
+    "hold for WC" in {
+      val prop = Monoid.monoidLaws(
+        Monoid.wcMonoid,
+        for {
+          b1 <- booleanGen
+          b2 <- booleanGen
+          randomStrSeed <- Gen.choose(1, 200000)
+          strLength <- Gen.choose(0, 10)
+          spaceLocation <- Gen.choose(0, 10)
+          numWords <- Gen.choose(0, 4)
+        } yield {
+          val createStub = b1 && b2
+          val r = new Random(randomStrSeed)
+          val str = r.nextString(strLength).replaceAll("""\s""", "")
 
-        if (createStub || str.isEmpty) {
-          Stub(str)
-        } else {
-          val safeSpaceLocation = spaceLocation.min(str.length)
-          Part(
-            str.substring(0, safeSpaceLocation),
-            numWords,
-            str.substring((safeSpaceLocation + 1).min(str.length))
-          )
+          if (createStub || str.isEmpty) {
+            Stub(str)
+          } else {
+            val safeSpaceLocation = spaceLocation.min(str.length)
+            Part(
+              str.substring(0, safeSpaceLocation),
+              numWords,
+              str.substring((safeSpaceLocation + 1).min(str.length))
+            )
+          }
         }
-      }
-    )
-    Prop.run(prop)
+      )
+      Prop.run(prop)
+    }
   }
 
-  "create WC" should "" in {
-    assert(Monoid.toWC("lorem ipsum do") === Part("lorem", 1, "do"))
-    assert(Monoid.toWC("lor sit amet, ") === Part("lor", 2, ""))
-    assert(Monoid.toWC("we") === Stub("we"))
-    assert(Monoid.toWC(" i") === Part("", 0, "i"))
-    assert(Monoid.toWC("i ") === Part("i", 0, ""))
-    assert(Monoid.toWC("") === Stub(""))
-    assert(Monoid.toWC(" ") === Stub(" "))
-  }
+  "countWords" in {
+    assert(Monoid.countWords("") == 0)
+    assert(Monoid.countWords("a") == 1)
+    assert(Monoid.countWords("a b") == 2)
+    assert(Monoid.countWords(" a b ") == 2)
+    assert(Monoid.countWords(" ab ") == 1)
 
-  "countWords" should "" in {
     val s =
       """When we add up the results of counting the words in these strings, we want to avoid
-        |double-counting the word dolor. Clearly, just counting the words as an Int isn’t sufficient.
+        |double-counting the word dolor. Clearly, just counting the words as an Int isn't sufficient.
         |We need to find a data structure that can handle partial results like the half words do and
         |lor, and can track the complete words seen so far, like ipsum, sit, and amet.""".stripMargin
-    val ans = Monoid.countWords(s)
-    assert(ans === 62)
+    assert(Monoid.countWords(s) == 64)
   }
 
-  "Foldable" should "toList" in {
-    val s = (1 to 5).to(LazyList)
-    val l = Foldable.lazyList.toList(s)
-    assert(l == List(1, 2, 3, 4, 5))
+  "Foldable" - {
+    "toList" in {
+      val s = (1 to 5).to(LazyList)
+      val l = Foldable.lazyList.toList(s)
+      assert(l == List(1, 2, 3, 4, 5))
+    }
   }
 
-  "monoid laws" should "hold for product" in {
-    val prop = Monoid.monoidLaws(
-      Monoid.productMonoid(Monoid.intAddition, Monoid.intMultiplication),
-      intGen ** intGen
-    )
-    Prop.run(prop)
+  "monoid laws" - {
+    "hold for product" in {
+      val prop = Monoid.monoidLaws(
+        Monoid.productMonoid(Monoid.intAddition, Monoid.intMultiplication),
+        intGen ** intGen
+      )
+      Prop.run(prop)
+    }
   }
 
-  "bag" should "" in {
+  "bag" in {
     val ans = Monoid.bag(Vector("a", "rose", "is", "a", "rose"))
     assert(ans === Map("a" -> 2, "rose" -> 2, "is" -> 1))
   }
